6b607a3a61d7c0033f227feba1d31f7edaac4057
Matt <mathew.beldon@hotmail.co.uk>
2023-03-12 14:34:49 +0000

    [+] Adds s3 endpoints for creating/listing/viewing a bucket [*] Updates docker config to connect to localstack via api

---
+++ Aws.Local.Example.Api/Controllers/S3Controller.cs
@@ -0,0 +1,42 @@
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aws.Local.Example.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class S3Controller : ControllerBase
+    {
+        private readonly IAmazonS3 _amazonS3;
+        public S3Controller(IAmazonS3 amazonS3)
+        {
+            _amazonS3 = amazonS3;
+        }
+
+        [HttpPost("bucket")]
+        public async Task<IActionResult> CreateBucket([FromBody] string bucketName)
+        {
+            var putBucketRequest = new PutBucketRequest()
+            {
+                BucketName = bucketName,
+                UseClientRegion = true
+            };
+            var putBucketResponse = await _amazonS3.PutBucketAsync(putBucketRequest);
+
+            return Ok(putBucketResponse);
+        }
+
+        [HttpGet("bucket")]
+        public async Task<IActionResult> GetBuckets(CancellationToken cancellationToken)
+        {
+            return Ok(await _amazonS3.ListBucketsAsync(cancellationToken));
+        }
+
+        [HttpGet("bucket/{bucketName}")]
+        public async Task<IActionResult> GetBuckets([FromRoute] string bucketName, CancellationToken cancellationToken)
+        {
+            return Ok(await _amazonS3.ListObjectsAsync(bucketName,cancellationToken));
+        }
+    }
+}
--- Aws.Local.Example.Api/Program.cs
+++ Aws.Local.Example.Api/Program.cs
@@ -1,3 +1,6 @@
+using Amazon;
+using Amazon.S3;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,18 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDefaultAWSOptions(builder.Configuration.GetAWSOptions());
+builder.Services.AddSingleton<IAmazonS3>(sc =>
+{
+    
+    var awsS3Config = new AmazonS3Config
+    {
+        RegionEndpoint = RegionEndpoint.GetBySystemName(builder.Configuration["AWS:Region"]),
+        ServiceURL = Environment.GetEnvironmentVariable("AWS_SERVICEURL") ?? builder.Configuration["AWS:ServiceURL"],
+        ForcePathStyle = bool.Parse(builder.Configuration["AWS:ForcePathStyle"]!)
+    };
+    return new AmazonS3Client(awsS3Config);
+});
 
 var app = builder.Build();
 
